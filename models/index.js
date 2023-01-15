c272dbd2080a88b79b15639d29d9a92b474bd91b
Marie Lewis <mariea1022@gmail.com>
2023-01-15 15:51:14 -0800

    added relationships for Comment

--- models/index.js
+++ models/index.js
@@ -3,13 +3,36 @@ const Blog = require('./Blog');
 const User = require('./User');
 const Comment = require('./Comment');
 
-// Products belongsTo Category
+// a blog belongs to a user
 Blog.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
-// Categories have many Products
+// a user can have many blogs
 User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+// a comment belongs to a blog
+Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id',
+});
+
+// a user can have many comments
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+// a comment belongs to a user
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+})
+
+// a blog can have many comments
+Blog.hasMany(Comment, {
   foreignKey: 'blog_id',
   onDelete: 'CASCADE',
 });
