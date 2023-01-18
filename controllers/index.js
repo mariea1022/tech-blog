07bb1c41c39581ed5617d0bbad39e7d4a6b118ef
Marie Lewis <mariea1022@gmail.com>
2023-01-17 17:54:29 -0800

    reorgnized order of routes, so '/' is in the beginning - cleaner code

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
+const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
 
-router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
