dd14a3456dcda0fed544e420b1b87a112e1be610
Marie Lewis <mariea1022@gmail.com>
2023-01-17 17:49:39 -0800

    modified code - checking to see if (username && password) is true instead

--- public/js/login.js
+++ public/js/login.js
@@ -4,23 +4,36 @@ const loginFormHandler = async (event) => {
   const password = document.getElementById("password-login").value.trim();
 
   // validate that both username and password were entered
-  if (!username || !password) {
-    alert("Please enter both a username or password");
-    return;
-  }
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+            username,
+            password
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 
-  const response = await fetch("/api/users/login", {
-    method: "POST",
-    body: JSON.stringify({ username, password }),
-    headers: { "Content-Type": "application/json" },
-  });
+//   if (!username || !password) {
+//     alert("Please enter both a username or password");
+//     return;
+//   }
+
+//   const response = await fetch("/api/users/login", {
+//     method: "POST",
+//     body: JSON.stringify({ username, password }),
+//     headers: { "Content-Type": "application/json" },
+//   });
 
   if (response.ok) {
     document.location.replace("/dashboard");
   } else {
-    alert(response.statusText);
+    console.log("err: ", response);
+    //alert(response.statusText);
   }
-};
+}
+}
 
-const loginFormEl = document.querySelector('.login-form');
-loginFormEl.addEventListener('submit', loginFormHandler);
+const loginFormEl = document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
