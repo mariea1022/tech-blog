d5036c1002836374ba2a8096f8b2ad8159a9b836
Marie Lewis <mariea1022@gmail.com>
2023-01-29 21:22:31 -0800

    added id's for sign up instead and log in instead buttons + front end js to redirect to those specific handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -5,35 +5,44 @@ const loginFormHandler = async (event) => {
 
   // validate that both username and password were entered
   if (username && password) {
-    const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-            username,
-            password
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
+    const response = await fetch("/api/users/login", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
 
-//   if (!username || !password) {
-//     alert("Please enter both a username or password");
-//     return;
-//   }
+    // if (!username || !password) {
+    //   alert("Please enter both a username or password");
+    //   return;
+    // }
 
-//   const response = await fetch("/api/users/login", {
-//     method: "POST",
-//     body: JSON.stringify({ username, password }),
-//     headers: { "Content-Type": "application/json" },
-//   });
+    // const response = await fetch("/api/users/login", {
+    //   method: "POST",
+    //   body: JSON.stringify({ username, password }),
+    //   headers: { "Content-Type": "application/json" },
+    // });
 
-  if (response.ok) {
-    document.location.replace("/dashboard");
-  } else {
-    console.log("err: ", response);
-    //alert(response.statusText);
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      console.log("err: ", response);
+      //alert(response.statusText);
+    }
   }
-}
-}
+};
 
-const loginFormEl = document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+const signupInsteadFormHandler = (event) => {
+  event.preventDefault();
+
+  document.location.replace("/signup");
+};
+
+document
+  .querySelector("#signup")
+  .addEventListener("click", signupInsteadFormHandler);
+document.querySelector("#login").addEventListener("click", loginFormHandler);
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,28 +1,35 @@
 async function signupFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = document.getElementById("username-login").value.trim();
-    const password = document.getElementById("password-login").value.trim();
+  const username = document.getElementById("username-login").value.trim();
+  const password = document.getElementById("password-login").value.trim();
 
-    if (username && password) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({
-                username,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-        if (response.ok) {
-            console.log('success');
+  if (username && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      console.log("success");
 
-
-            document.location.replace('/dashboard');
-
-        } else {
-            alert(response.statusText);
-        }
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
     }
+  }
 }
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+const loginInsteadFormHandler = (event) => {
+  event.preventDefault();
+
+  document.location.replace("/login");
+};
+
+document
+  .querySelector("#login")
+  .addEventListener("click", loginInsteadFormHandler);
+document.querySelector("#signup").addEventListener("click", signupFormHandler);
