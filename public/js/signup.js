54360bc17a034b67bd6c5f0bb1d3cf917728db38
Marie Lewis <mariea1022@gmail.com>
2023-01-29 20:10:48 -0800

    added sign up, which if successful, user will be taken to dashboard

---
+++ public/js/signup.js
@@ -0,0 +1,28 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username-login").value.trim();
+    const password = document.getElementById("password-login").value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            console.log('success');
+
+
+            document.location.replace('/dashboard');
+
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
