b96642aee203132af764b5fba3c2fd85dc3836ad
Marie Lewis <mariea1022@gmail.com>
2023-03-04 17:54:42 -0800

    replaced created_at with date

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -1,132 +1,135 @@
-const router = require('express').Router();
-const { Blog, User, Comment } = require('../../models');
+const router = require("express").Router();
+const { Blog, User, Comment } = require("../../models");
 
-const withAuth = require('../../utils/auth');
+const withAuth = require("../../utils/auth");
 
 // Get all posts
 router.get("/", (req, res) => {
-    Blog.findAll({
-        attributes: ["id", "blog_content", "blog_name", "date"],
-        order: [
-            ["created_at", "DESC"]
-        ],
-        include: [{
-            model: User,
-            attributes: ["username"],
+  Blog.findAll({
+    attributes: ["id", "blog_content", "blog_name", "date"],
+    order: [["date", "DESC"]],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_content", "blog_id", "user_id", "date"],
+        include: {
+          model: User,
+          attributes: ["username"],
         },
-        {
-            model: Comment,
-            attributes: ["id", "comment_content", "blog_id", "user_id", "date"],
-            include: {
-                model: User,
-                attributes: ["username"],
-            },
-        },
-        ],
-    })
-        .then((dbPostData) => res.json(dbPostData))
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // Get a single post
 router.get("/:id", (req, res) => {
-    Blog.findOne({
-        where: {
-            id: req.params.id,
+  Blog.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "blog_content", "blog_name", "date"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_content", "blog_id", "user_id", "date"],
+        include: {
+          model: User,
+          attributes: ["username"],
         },
-        attributes: ["id", "blog_content", "blog_name", "date"],
-        include: [{
-            model: User,
-            attributes: ["username"],
-        },
-        {
-            model: Comment,
-            attributes: ["id", "comment_content", "blog_id", "user_id", "date"],
-            include: {
-                model: User,
-                attributes: ["username"],
-            },
-        },
-        ],
-    })
-        .then((dbPostData) => {
-            if (!dbPostData) {
-                res.status(404).json({
-                    message: "No post found with this id"
-                });
-                return;
-            }
-            res.json(dbPostData);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({
+          message: "No post found with this id",
         });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // Create a post
 router.post("/", withAuth, (req, res) => {
-    console.log("creating a post");
-    Blog.create({
-        blog_name: req.body.blog_name,
-        blog_content: req.body.blog_content,
-        user_id: req.session.user_id
-    })
-        .then((dbPostData) => res.json(dbPostData))
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+  console.log("*** creating a post");
+  Blog.create({
+    blog_name: req.body.blog_name,
+    blog_content: req.body.blog_content,
+    user_id: req.session.user_id,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // Update a post
 router.put("/:id", withAuth, (req, res) => {
-    Blog.update({
-        blog_name: req.body.blog_name,
-        blog_content: req.body.blog_content,
-    }, {
-        where: {
-            id: req.params.id,
-        },
-    })
-        .then((dbPostData) => {
-            if (!dbPostData) {
-                res.status(404).json({
-                    message: "No post found with this id"
-                });
-                return;
-            }
-            res.json(dbPostData);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
+  Blog.update(
+    {
+      blog_name: req.body.blog_name,
+      blog_content: req.body.blog_content,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({
+          message: "No post found with this id",
         });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // Delete a post
 router.delete("/:id", withAuth, (req, res) => {
-    BLog.destroy({
-        where: {
-            id: req.params.id,
-        },
-    })
-        .then((dbPostData) => {
-            if (!dbPostData) {
-                res.status(404).json({
-                    message: "No post found with this id"
-                });
-                return;
-            }
-            res.json(dbPostData);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
+  Blog.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({
+          message: "No post found with this id",
         });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
